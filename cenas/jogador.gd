072f9f4b557c33762179c6af548e217699c8b47e
Samuel O. <sam.morais2025@gmail.com>
2026-08-25 15:19:17 -0300

    Incrementação do script do personagem

--- cenas/jogador.gd
+++ cenas/jogador.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+var velocidade_pulo = -400
+func _process(delta: float) -> void:
+	velocity += get_gravity()*delta
+
+	if is_on_floor():
+		$Imagem.play("correr")
+		if Input.is_action_pressed("ui_select"):
+			velocity.y = velocidade_pulo
+	else:
+		$Imagem.play("pular")
+	move_and_slide()
